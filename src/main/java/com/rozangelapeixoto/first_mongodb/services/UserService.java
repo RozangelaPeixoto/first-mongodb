5098daa3e31f2fdd5a1e91825f328483040a1972
RozangelaPeixoto <roxpmelo@gmail.com>
2024-12-22 19:46:48 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/rozangelapeixoto/first_mongodb/services/UserService.java
@@ -0,0 +1,21 @@
+package com.rozangelapeixoto.first_mongodb.services;
+
+import com.rozangelapeixoto.first_mongodb.domain.User;
+import com.rozangelapeixoto.first_mongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+
+}
