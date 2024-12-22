125cd2d9766758f7eae967e135203e4ad50c8ee2
RozangelaPeixoto <roxpmelo@gmail.com>
2024-12-22 17:29:55 -0300

    Projeto criado

---
+++ src/main/java/com/rozangelapeixoto/first_mongodb/FirstMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.rozangelapeixoto.first_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rozangelapeixoto/first_mongodb/FirstMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozangelapeixoto.first_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
