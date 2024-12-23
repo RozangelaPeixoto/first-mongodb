abc95da5b636acf0f9b2ffd49baa8f41cf1b98a9
RozangelaPeixoto <roxpmelo@gmail.com>
2024-12-23 14:09:35 -0300

    Atualização de usuário com PUT

--- src/main/java/com/rozangelapeixoto/first_mongodb/services/UserService.java
+++ src/main/java/com/rozangelapeixoto/first_mongodb/services/UserService.java
@@ -36,6 +36,21 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj){
+        if(!repo.existsById(obj.getId())){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        Optional<User> optionalObj = repo.findById(obj.getId());
+        User newObj = repo.findById(obj.getId()).get();
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
